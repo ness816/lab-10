04103a94355e605c4e800af06633a26d4d53d32a
ness816 <ness41861@gmail.com>
2023-12-19 17:04:33 +0700

    กลับละนะ

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sum_of_square = 0;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+	{
+		sum += atof(textline.c_str());
+        sum_of_square += pow(atof(textline.c_str()),2);
+        count++;
+	}
+    source.close();
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum / count << endl;
+    cout << "Standard deviation = " << sqrt ((sum_of_square/count)-((sum*sum)/(count*count)));
+}
